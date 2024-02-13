2d7a92e186ac48725b98b332c113a6ccd53bcd41
arammes03 <arammes03@gmail.com>
2024-02-13 19:17:15 +0100

    Pagina login y ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// IMPORT COMPONENTS
+import { LoginComponent } from './pages/login/login.component';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
