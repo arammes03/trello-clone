4f85f10bb714e1be5d8f069f946d8325e793d22d
arammes03 <arammes03@gmail.com>
2024-02-16 16:28:21 +0100

    cambios generales de routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,14 @@
 import { Routes } from '@angular/router';
 
-// IMPORT COMPONENTS
-import { LoginComponent } from './pages/login/login.component';
-import { BoardsComponent } from './pages/boards/boards.component';
-import { BoardComponent } from './pages/board/board.component';
-import { ScrollComponent } from './pages/scroll/scroll.component';
-import { TableComponent } from './pages/table/table.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ForgotPassComponent } from './pages/forgot-pass/forgot-pass.component';
-
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
+    loadChildren: () =>
+      import('./modules/auth/auth.routes').then((m) => m.authRoutes),
   },
   {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'forgot-password',
-    component: ForgotPassComponent,
-  },
-  {
-    path: 'boards',
-    component: BoardsComponent,
-  },
-  {
-    path: 'board',
-    component: BoardComponent,
-  },
-  {
-    path: 'scroll',
-    component: ScrollComponent,
-  },
-  {
-    path: 'table',
-    component: TableComponent,
+    path: 'app',
+    loadChildren: () =>
+      import('./modules/layout/layout.routes').then((m) => m.layoutRoutes),
   },
 ];
