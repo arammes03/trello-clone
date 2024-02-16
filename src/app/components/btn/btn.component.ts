d2954ec64c3ee5d4326e6ef5cb56106b8179f7ab
arammes03 <arammes03@gmail.com>
2024-02-16 13:22:27 +0100

    mod funcionamiento boton

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -9,5 +9,29 @@ import { NgClass } from '@angular/common';
 })
 export class BtnComponent {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() btnColor = 'success';
+  @Input() btnColor: 'success' | 'primary' | 'light' = 'success';
+
+  mapColors = {
+    success: {
+      'bg-success': true,
+      'hover:bg-hover': true,
+      'text-white': true,
+    },
+    primary: {
+      'bg-blue': true,
+      'hover:bg-hoverBlue': true,
+      'text-white': true,
+    },
+    light: {
+      'bg-gray-200': true,
+      'hover:bg-gray-400': true,
+      'text-gray-700': true,
+    },
+  };
+
+  get colors() {
+    const colors = this.mapColors[this.btnColor];
+    if (colors) return colors;
+    else return {};
+  }
 }
