5af69bd0e21d3dae75bfa2c470c17d5fc6e931da
arammes03 <arammes03@gmail.com>
2024-02-19 17:13:45 +0100

    mod btnComponent añadiendo que si el estado es 'loading' salga un spinner

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -1,15 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { NgClass } from '@angular/common';
 
+// ICONS
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-btn',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, FontAwesomeModule],
   templateUrl: './btn.component.html',
 })
 export class BtnComponent {
+  @Input() disabled = false;
+  @Input() loading = false;
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
   @Input() btnColor: 'success' | 'primary' | 'light' = 'success';
+
+  // ICONS
+  faSpinner = faSpinner;
 
   mapColors = {
     success: {
